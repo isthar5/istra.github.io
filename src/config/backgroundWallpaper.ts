2697a9f88415d8806e55c19081860c8c7fc8ed37
isthar5 <isthar5@gmail.com>
2026-09-07 23:32:30 +0800

    更换头像和背景图

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -8,9 +8,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	// 背景图片配置
 	src: {
 		// 桌面背景图片
-		desktop: "/assets/images/d1.webp",
+		desktop: "/assets/images/d1.jpg",
 		// 移动背景图片
-		mobile: "/assets/images/m1.webp",
+		mobile: "/assets/images/m1.jpg",
 	},
 	// Banner模式特有配置
 	banner: {
--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -2,7 +2,7 @@ import type { ProfileConfig } from "../types/config";
 
 export const profileConfig: ProfileConfig = {
 	// 头像
-	avatar: "/assets/images/avatar.webp",
+	avatar: "/assets/images/avatar.png",
 
 	// 名字
 	name: "Firefly",
