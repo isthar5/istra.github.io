12b248f31b74dda6b192318d567e247cb8be40a9
isthar5 <isthar5@gmail.com>
2026-09-07 23:48:01 +0800

    导航栏链接换成个人GitHub，删除作者B站链接

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -28,15 +28,9 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/isthar5",
 				external: true,
 				icon: "fa6-brands:github",
-			},
-			{
-				name: "Bilibili",
-				url: "https://space.bilibili.com/38932988",
-				external: true,
-				icon: "fa6-brands:bilibili",
 			},
 		],
 	});
