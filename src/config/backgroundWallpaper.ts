cf9e953b5a3401e890be643720010711232032cd
isthar5 <isthar5@gmail.com>
2026-09-12 15:37:16 +0800

    更换背景图和头像，删除音乐播放器和Pixiv来源链接

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -8,9 +8,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 	// 背景图片配置
 	src: {
 		// 桌面背景图片
-		desktop: "/assets/images/d1.jpg",
+		desktop: "/assets/images/d1.webp",
 		// 移动背景图片
-		mobile: "/assets/images/m1.jpg",
+		mobile: "/assets/images/m1.webp",
 	},
 	// Banner模式特有配置
 	banner: {
@@ -55,9 +55,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		credit: {
 			enable: {
 				// 桌面端显示横幅图片来源文本
-				desktop: true,
+				desktop: false,
 				// 移动端显示横幅图片来源文本
-				mobile: true,
+				mobile: false,
 			},
 			text: {
 				// 桌面端要显示的来源文本
